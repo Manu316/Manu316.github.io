7c8de19f5724a842deb0fad4e5ca930a9a0dd1dc
Manu316 <zS22017014@estudiantes.uv.mx>
2025-08-29 15:45:38 -0600

    Se agregó hora en tiempo real

--- js/script.js
+++ js/script.js
@@ -25,4 +25,29 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
+
+    function updateTime() {
+        const now = new Date();
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        };
+        const formattedTime = now.toLocaleDateString('es-ES', options);
+        const datetimeElement = document.getElementById('datetime');
+        if (datetimeElement) {
+            datetimeElement.textContent = formattedTime;
+        }
+    }
+
+    setInterval(updateTime, 1000);
+    updateTime();
+
 });
+
+
+
