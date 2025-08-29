701ecc6ef383000db8cd1b70d6dbe0dfd912d57f
Manu316 <zS22017014@estudiantes.uv.mx>
2025-08-28 22:34:28 -0600

    Secciones separadas en diferentes archivos html

--- js/script.js
+++ js/script.js
@@ -1,33 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Almacena todas las pantallas para una navegación más fácil
-    const introScreen = document.getElementById('intro-screen');
-    const aboutMeScreen = document.getElementById('about-me');
-    const hobbiesScreen = document.getElementById('hobbies');
 
-    // Función para mostrar solo una pantalla a la vez
-    window.showScreen = (screenId) => {
-        const screens = [
-            introScreen,
-            aboutMeScreen,
-            hobbiesScreen
-        ];
-
-        screens.forEach(screen => {
-            if (screen && screen.id === screenId) {
-                screen.classList.remove('hidden');
-                screen.classList.add('visible');
-            } else if (screen) {
-                screen.classList.remove('visible');
-                screen.classList.add('hidden');
-            }
-        });
-    };
-
-    showScreen('intro-screen');
-
-    // Función para desvanecer la música antes de navegar
     function fadeOutMusic(url) {
-
         const player = document.getElementById('spotify-player');
         if (player) {
             player.style.opacity = 0.5;
@@ -35,15 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 window.location.href = url;
             }, 500);
         } else {
-            window.location.href = url;
+            window.location.href = url; 
         }
     }
 
     document.querySelectorAll('a').forEach(link => {
         link.addEventListener('click', function(event) {
             const href = this.getAttribute('href');
+            
+            // Verifica si el enlace no es externo (http) y no es un ancla (#)
             if (href && !href.startsWith('http') && !href.startsWith('#')) {
-                event.preventDefault();
+                // Previene la acción por defecto del enlace
+                event.preventDefault(); 
+                
                 fadeOutMusic(href);
             }
         });
